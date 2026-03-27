c44d5bbddfa81baa12e1e19342e5b1e2c14d1463
Piotr Rzeczkowski <pr@riveolabs.com>
2026-03-27 15:53:37 +0100

    Add prettier config

---
+++ src/configs/prettier.js
@@ -0,0 +1,6 @@
+import eslintConfigPrettierFlat from 'eslint-config-prettier/flat';
+
+/**
+ * @type {import('eslint').Linter.Config[]}
+ */
+export const prettierConfig = [eslintConfigPrettierFlat];
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { baseConfig } from './configs/base.js';
 import { importConfigs } from './configs/import.js';
+import { prettierConfig } from './configs/prettier.js';
 import { typescriptConfigs } from './configs/typescript.js';
 
-export const recommended = [baseConfig, importConfigs, typescriptConfigs];
+export const recommended = [
+  baseConfig,
+  importConfigs,
+  typescriptConfigs,
+  prettierConfig,
+];
