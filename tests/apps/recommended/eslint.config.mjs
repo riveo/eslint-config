516cf62e8fd482924460ccabaaa2027cf7e9f92c
Piotr Rzeczkowski <pr@riveolabs.com>
2026-04-10 12:28:28 +0200

    Configure globals in tests

--- tests/apps/recommended/eslint.config.mjs
+++ tests/apps/recommended/eslint.config.mjs
@@ -1,7 +1,13 @@
 import { configs } from '@riveo/eslint-config';
 import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 
 export default defineConfig(
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
   {
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
