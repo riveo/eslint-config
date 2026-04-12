0f7def6787c6ba7fca737c5aa64b64e34c341225
Piotr Rzeczkowski <pr@riveolabs.com>
2026-04-12 12:40:49 +0200

    Remove duplicate rules
    Since this config is typescript first, non-typechecked rules apply to js files too. No need to restore them

--- src/configs/javascript.js
+++ src/configs/javascript.js
@@ -1,5 +1,4 @@
 import js from '@eslint/js';
-import { javascriptTSEslintRules } from './javascript-tseslint-rules.js';
 
 const jsExtensions = ['.js', '.cjs', '.mjs', '.jsx'];
 
@@ -8,14 +7,7 @@ const jsExtensions = ['.js', '.cjs', '.mjs', '.jsx'];
  */
 export const javascriptConfig = {
   name: 'riveo/javascript',
-  extends: [
-    { name: '@eslint/js/recommended', ...js.configs.recommended },
-    {
-      files: jsExtensions.map((ext) => `**/*${ext}`),
-      name: 'riveo/javascript-tseslint-rules',
-      rules: javascriptTSEslintRules,
-    },
-  ],
+  extends: [{ name: '@eslint/js/recommended', ...js.configs.recommended }],
   settings: {
     // in some cases the tseslint parser fails to work with js.
     // switch js files to espree ensures that raw js files doesn't go through
