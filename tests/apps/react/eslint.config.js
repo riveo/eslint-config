1c5a0d0591731e4d9b4c28e792dc0b47ca7d76ae
Piotr Rzeczkowski <pr@riveolabs.com>
2026-04-25 11:45:09 +0200

    Reconfigure eslint in react test app

--- tests/apps/react/eslint.config.js
+++ tests/apps/react/eslint.config.js
@@ -1,22 +1,17 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { configs } from '@riveo/eslint-config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
-      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-])
+  configs.recommended,
+  configs.react,
+]);
