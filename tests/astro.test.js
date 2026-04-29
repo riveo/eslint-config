2961b95fa3554d506cbb6080c341981acd3046b6
Piotr Rzeczkowski <pr@riveolabs.com>
2026-04-29 12:27:46 +0200

    Cleanup astro test suite

--- tests/astro.test.js
+++ tests/astro.test.js
@@ -1,19 +1,11 @@
 import path from 'node:path';
-import { describe, it, before } from 'node:test';
+import { describe, it } from 'node:test';
 import { createLintSuite } from './helpers/create-lint-suite.js';
 
 describe('astro config', () => {
   const appDir = path.join(import.meta.dirname, 'apps/astro');
 
   const { testSmokeFiles, testExpectedFailures } = createLintSuite(appDir);
-
-  // astro plugin has some strange way of finding if @typescript-eslint/parser
-  // is available. With the pnpm setup we need to be in the app's dir and the app
-  // has to use `@typescript-eslint/parser` as a direct dependency. In the release
-  // this is not needed.
-  before(() => {
-    process.chdir(appDir);
-  });
 
   it(
     'lints smoke files without errors',
